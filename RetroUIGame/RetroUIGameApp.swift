83b31855a42bb7305ca0cc7c1f41be79a551926d
heibalvin <heibalvin@gmail.com>
2024-05-25 11:35:57 +0400

    Initial Commit

---
+++ RetroUIGame/RetroUIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroUIGameApp.swift
+//  RetroUIGame
+//
+//  Created by Alvin Heib on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RetroUIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
